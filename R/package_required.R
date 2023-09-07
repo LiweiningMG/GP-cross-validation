f797826a7b657f802ffbe6e026d05eb0d4e1e089
LiweiningMG <liwn@cau.edu.cn>
2023-09-07 23:27:42 +0800

    23.9.7

--- R/package_required.R
+++ R/package_required.R
@@ -1,7 +1,7 @@
-#!/work/apps/tools/conda/minconda3/20230202/bin/Rscript
+#!/home/zyzzuser01/miniconda3/envs/R-4.2.2/bin/Rscript
 
 ## 检查需要的R包是否都已安装  ##
-## liwn 2023-07-05 ##
+## liwn 2023-09-07 ##
 
 # 要检查的 R 包列表
 packages <- c("data.table", "LAVA", "getopt", "MASS", "RcppEigen", "Matrix", "ggplot2", "reshape2",
@@ -10,8 +10,9 @@ packages <- c("data.table", "LAVA", "getopt", "MASS", "RcppEigen", "Matrix", "gg
 # 检查并安装缺失的包
 missing_packages <- packages[!packages %in% installed.packages()]
 if (length(missing_packages) > 0) {
-  message("以下包未安装：")
-  message(missing_packages)
-  message("请在R语言中运行以下命令安装：")
-  message(paste0("install.packages(\"", missing_packages, "\")"))
+  message("以下R包未安装: ")
+  cat(missing_packages, sep = "\n")
+  message("请在R语言中运行以下命令安装: ")
+  command <- paste0("install.package(\"", paste(missing_packages, collapse = "\", \""), "\")")
+  cat(command, "\n")
 }
